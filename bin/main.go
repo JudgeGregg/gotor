4f3dffae6e783d5d6b6fc65f49424e9704f2d1bc
Grégoire Juge <gregoire.juge@assemblee-nationale.fr>
2022-05-13 14:07:24 +0200

    Manage partial line with tail

--- bin/main.go
+++ bin/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"bufio"
 	"flag"
+	"fmt"
 	"io"
 	"os"
 	"path"
+	"strings"
 	"time"
 
 	"github.com/JudgeGregg/gotor/globals"
@@ -52,16 +54,43 @@ func tail(filename string, lines chan string) {
 		panic(err)
 	}
 	oldSize := info.Size()
+	tempLine := ""
+	str := ""
 	for {
-		for line, prefix, err := r.ReadLine(); err != io.EOF; line, prefix, err = r.ReadLine() {
-			if prefix {
-				str, _, _ := transform.String(transformer, string(line))
-				lines <- str
-			} else {
-				str, _, _ := transform.String(transformer, string(line))
-				lines <- str
+		for line, err := r.ReadString('\n'); ; line, err = r.ReadString('\n') {
+			fmt.Print("baseline: ", line)
+			if err == io.EOF {
+				// we have a "temp" line
+				if line != "" {
+					// not empty
+					if !strings.Contains(line, "\n") {
+						// partial line, wait
+						tempLine += line
+						fmt.Print("tempLine: ", tempLine)
+						continue
+					} else {
+						fmt.Print("end of line!")
+						// end of partial line, proceed
+						tempLine += line
+						fmt.Print("tempLine: ", tempLine)
+					}
+				} else {
+					//empty temp line = "EOF"
+					break
+				}
 			}
+			if tempLine != "" {
+				tempLine += line
+				fmt.Print("tempLine: ", tempLine)
+				str, _, _ = transform.String(transformer, string(tempLine))
+				tempLine = ""
+			} else {
+				fmt.Print("line: ", line)
+				str, _, _ = transform.String(transformer, string(line))
+			}
+			lines <- str
 		}
+		fmt.Print("DONE")
 		pos, err := f.Seek(0, io.SeekCurrent)
 		if err != nil {
 			panic(err)
